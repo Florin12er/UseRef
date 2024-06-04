39474f1d725f8cdd367934227d455cac0e1b93e7
florin <apetreiflorin236@gmail.com>
2024-06-04 14:52:00 +0200

    learned how to use useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,65 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, useMemo } from "react";
+
+function slowFunction(num) {
+  console.log("hello from romania");
+  for (let i = 0; i <= 1000000; i++) {}
+  return num * 2;
+}
 
 function App() {
-    const [name, setName] = useState("");
-    const renderCount = useRef(0);
-    const inputRef = useRef();
-    const prevName = useRef("");
-    useEffect(() => {
-        renderCount.current = renderCount.current + 1;
-    });
-    function focus() {
-        inputRef.current.focus();
-    }
-    useEffect(() => {
-        prevName.current = name;
-    }, [name]);
+  const [number, setNumber] = useState(0);
+  const [dark, setDark] = useState(false);
+  const doubleNumber = useMemo(() => {
+    return slowFunction(number);
+  }, [number]);
+  const themeStyles = useMemo(() => {
+    return {
+      backgroundColor: dark ? "black" : "white",
+      color: dark ? "white" : "black",
+    };
+  }, [dark]);
 
-    return (
-        <>
-            <input
-                ref={inputRef}
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-            />
-            <div>
-                My password is {name} and used to be {prevName.current}
-            </div>
-            <div>I rendered {renderCount.current} times</div>
-            <button onClick={focus}>Focus</button>
-        </>
-    );
+  useEffect(() => {
+    console.log("Theme changed");
+  }, [themeStyles]);
+
+  const [name, setName] = useState("");
+  const renderCount = useRef(0);
+  const inputRef = useRef();
+  const prevName = useRef("");
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+  });
+  function focus() {
+    inputRef.current.focus();
+  }
+  useEffect(() => {
+    prevName.current = name;
+  }, [name]);
+
+  return (
+    <>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(parseInt(e.target.value))}
+      />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>
+        Change theme
+      </button>
+      <div style={themeStyles}>{doubleNumber}</div>
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <div>
+        My password is {name} and used to be {prevName.current}
+      </div>
+      <div>I rendered {renderCount.current} times</div>
+      <button onClick={focus}>Focus</button>
+    </>
+  );
 }
 
 export default App;
