018020eda607ca439512ac8ff14407856a250bfd
florin <apetreiflorin236@gmail.com>
2024-06-04 21:30:15 +0200

    learned how to use useCallback and useId

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Refs are used for accesing doom elements and persistent values across render without causing a rerender.
+## useMemo
+
+- useMemo is a React hook that memoizes the 
+result of a function, recomputing it only when its dependencies change.
+This helps in optimizing performance by preventing unnecessary recalculations.
+- for slow functions and referencial equality
+
+## useCallback
+
+- useCallback is a React hook that memoizes a callback function, recreating it only when its dependencies change.
+This is useful to avoid unnecessary re-renders when passing callbacks to child components.
+- for referencial equality
+
+## useRef
+- useRef is a React hook that returns a mutable ref object. This object persists for the full lifetime of the component.
+It is commonly used to access DOM elements directly or to keep a mutable variable that does not trigger a re-render when changed. 
+
+## useId 
+- gives random id everytime you rerender the page, if the page renders the same as before than it will be the same id everytime
--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
-import { useState, useEffect, useRef, useMemo } from "react";
+import {
+  useState,
+  useEffect,
+  useRef,
+  useMemo,
+  useCallback,
+  useId,
+} from "react";
+
+import List from "./List";
 
 function slowFunction(num) {
-  console.log("hello from romania");
   for (let i = 0; i <= 1000000; i++) {}
   return num * 2;
 }
-
 function App() {
   const [number, setNumber] = useState(0);
   const [dark, setDark] = useState(false);
+  const id = useId();
+
+  const getItems = useCallback(
+    (Incremental) => {
+      return [
+        number + Incremental,
+        number + 1 + Incremental,
+        number + 2 + Incremental,
+      ];
+    },
+    [number],
+  );
+
+  const theme = {
+    backgroundColor: dark ? "#333" : "#FFF",
+    color: dark ? "#FFF" : "#333",
+  };
   const doubleNumber = useMemo(() => {
     return slowFunction(number);
   }, [number]);
+
   const themeStyles = useMemo(() => {
     return {
       backgroundColor: dark ? "black" : "white",
@@ -19,16 +44,16 @@ function App() {
     };
   }, [dark]);
 
-  useEffect(() => {
-    console.log("Theme changed");
-  }, [themeStyles]);
+  useEffect(() => {}, [themeStyles]);
 
   const [name, setName] = useState("");
   const renderCount = useRef(0);
+    const ID = useRef()
   const inputRef = useRef();
   const prevName = useRef("");
   useEffect(() => {
     renderCount.current = renderCount.current + 1;
+    console.log(ID)
   });
   function focus() {
     inputRef.current.focus();
@@ -39,6 +64,17 @@ function App() {
 
   return (
     <>
+      <div style={theme}>
+        <input
+          type="number"
+          value={number}
+          onChange={(e) => setNumber(parseInt(e.target.value))}
+        />
+        <button onClick={() => setDark((prevDark) => !prevDark)}>
+          Toggle theme
+        </button>
+        <List getItems={getItems} />
+      </div>
       <input
         type="number"
         value={number}
@@ -58,6 +94,7 @@ function App() {
       </div>
       <div>I rendered {renderCount.current} times</div>
       <button onClick={focus}>Focus</button>
+      <h1 ref={ID} id={id}>Hello</h1>
     </>
   );
 }
